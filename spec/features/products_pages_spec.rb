eb0a03cdb0257308b0f685bf1cb9fb9d31f954b0
Karla Buckner <kmb2662@gmail.com>
2017-11-17 15:53:30 -0800

    add styling, image, and pass 2 more capybara specs

--- spec/features/products_pages_spec.rb
+++ spec/features/products_pages_spec.rb
@@ -8,3 +8,27 @@ describe 'visit root route to product path' do
     expect(page).to have_content 'Avocado'
   end
 end
+
+describe 'visiting new product page and adding a product' do
+  it 'navigates to the new product page' do
+    visit products_path
+    click_link 'Add a New Product'
+    fill_in 'Name', :with => 'Avocado'
+    fill_in 'Cost', :with => '2'
+    fill_in 'Country', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'Avocado'
+  end
+end
+
+describe 'visiting the product edit page, editing a product' do
+  it 'navigates to the product edit page' do
+    product = Product.create(:name => 'Avocado', :cost => 2, :country =>'USA')
+    visit products_path(product)
+    first(:link, 'Avocado').click
+    click_link 'Edit'
+    fill_in 'Name', :with => 'Organic Avocado'
+    click_on 'Update Product'
+    expect(page).to have_content product.name
+  end
+end
--- spec/features/reviews_pages_spec.rb
+++ spec/features/reviews_pages_spec.rb
@@ -9,3 +9,17 @@ describe 'visit product path, route to review details' do
     expect(page).to have_content review.author
   end
 end
+
+describe 'visit root route to product path, add new review' do
+  it 'navigates from root to product path' do
+    product = Product.create(:name => 'Avocado', :cost => 2, :country =>'USA')
+    visit products_path
+    first(:link, 'Avocado').click
+    click_link 'Add a review'
+    fill_in 'Author', :with => "Karla"
+    fill_in 'Content', :with => "Avocado is the best thing in the whole entire world, I just love it so very much!"
+    fill_in 'Rating', :with => 5
+    click_on 'Create Review'
+    expect(page).to have_content "Avocado is the best thing in the whole entire world, I just love it so very much!"
+  end
+end
